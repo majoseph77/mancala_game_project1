eccf328809125005d947f5fd9d95f1489fbcb237
Meghan Joseph <majoseph77@aol.com>
2015-07-07 11:23:19 -0700

    board playerSide computerSide defined

--- html_boilerplate/js/main.js
+++ html_boilerplate/js/main.js
@@ -6,3 +6,6 @@ var gameIsBeingPlayed = false; // true
 var nextTurn = "X";            // "O"
 
 var board = $('.pit, .large_pit');
+
+var playerSide = $('#a, #1, #2, #3, #4, #5, #6');
+var computerSide= $('#7, #8, #9, #10, #11, #12, #b');
